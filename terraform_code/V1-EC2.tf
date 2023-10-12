eba24365f2fd5e34a24f4dc0002a2fcc73dfb42b
rajgit67 <rajeev.oracle@gmail.com>
2023-10-11 21:58:23 -0400

    commiting V1-EC2.tf file

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-  region = "us-east-1"
+ provider "aws" {
+  region = "us-east-2"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-036f5574583e16426"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "DevOps_Project_Key"
 }
